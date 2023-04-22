666cd27f2018748e2ef0c05ccb5e8d684c59745b
Ajeesh Babu Raveendra <131538086+ajeeshbaburaveendra8086@users.noreply.github.com>
2023-04-23 00:39:02 +0530

    Add files via upload

---
+++ calci/calci.js
@@ -0,0 +1,17 @@
+
+  var screen=document.querySelector('#screen');
+  var btn=document.querySelectorAll('.btn');
+
+
+  for(item of btn){
+    item.addEventListener('click',(e)=>{
+        btntext=e.target.innerText;
+        if(btntext==='x'){
+            btntext='*';
+        }
+        if(btntext==='/'){
+            btntext='/';
+        }
+        screen.value+=btntext
+    })
+  }
